e5167dde0be71bb994930b1bc57e998e188554cd
michael-devbsb <michael.devbsb@gmail.com>
2025-02-18 00:29:49 -0300

    Deploy do site para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  build: {
+    outDir: 'dist',
+  },
   base: 'michael-devbsb.github.io/nozmermo.github.io/',
   server: {
     host: "::",
